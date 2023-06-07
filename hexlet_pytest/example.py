1806d0946ceed07988a4194be6e1eadf61b55c0e
OlegKhiretdinov <oleg@oleg.win>
2023-06-07 21:17:20 +0300

    feat: add gitHook

--- hexlet_pytest/example.py
+++ hexlet_pytest/example.py
@@ -1,2 +1,15 @@
 def reverse(string):
+    """
+    This is the "example" module.
+
+    The example module supplies one function, reverse().  For example,
+
+    >>> reverse('awesome!')
+    '!emosewa'
+    """
     return string[::-1]
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
